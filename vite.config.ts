33481502aaa0398d8a8ff1b159fcf54cbc08b170
by <2540263991@qq.com>
2026-04-22 21:38:59 +0800

    refactor(vite.config.ts): 优化分包配置和插件命名
    - 重命名 Optimization 为 UniOptimization，统一插件命名
    - 增加 isMpWeixin 条件判断，仅在微信小程序端开启 optimization 修复运行到支付宝小程序报错的问题
    - 调整插件引入顺序，提高可读性

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from 'node:path'
 import process from 'node:process'
 import Uni from '@uni-helper/plugin-uni'
+import { isMpWeixin } from '@uni-helper/uni-env'
 import Components from '@uni-helper/vite-plugin-uni-components'
 // @see https://uni-helper.js.org/vite-plugin-uni-layouts
 import UniLayouts from '@uni-helper/vite-plugin-uni-layouts'
@@ -11,11 +12,12 @@ import UniPages from '@uni-helper/vite-plugin-uni-pages'
 // @see https://github.com/uni-helper/vite-plugin-uni-platform
 // 需要与 @uni-helper/vite-plugin-uni-pages 插件一起使用
 import UniPlatform from '@uni-helper/vite-plugin-uni-platform'
+
 /**
  * 分包优化、模块异步跨包调用、组件异步跨包引用
  * @see https://github.com/uni-ku/bundle-optimizer
  */
-import Optimization from '@uni-ku/bundle-optimizer'
+import UniOptimization from '@uni-ku/bundle-optimizer'
 // https://github.com/uni-ku/root
 import UniKuRoot from '@uni-ku/root'
 import dayjs from 'dayjs'
@@ -78,12 +80,8 @@ export default defineConfig(({ command, mode }) => {
         dts: 'src/types/uni-pages.d.ts',
       }),
       // Optimization 插件需要 page.json 文件，故应在 UniPages 插件之后执行
-      Optimization({
-        enable: {
-          'optimization': true,
-          'async-import': true,
-          'async-component': true,
-        },
+      UniOptimization({
+        enable: isMpWeixin,
         dts: {
           base: 'src/types',
         },
